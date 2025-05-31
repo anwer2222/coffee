ad074dd9a132efbf33890b8c4140f12b5d01682c
Anwar Fall <anwar2222@hotmail.com>
2025-05-31 18:32:10 -0400

    01: add Hero

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import CollectionPage from "@/components/Collection";
 import { Header } from "@/components/Header";
+import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <Header/>
+      <Hero/>
       <CollectionPage/>
 
 
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,22 +1,18 @@
-import styles from "../styles/Hero.module.scss";
 
-const Hero = ({ imageClassName, title, description }) => {
+
+const Hero = () => {
+
+ 
   return (
-    <>
-      <section className={styles.container + ` ${imageClassName}`}>
-        <div className={styles.text_content}>
-          <h1>{title}</h1>
-          <p>{description}</p>
-        </div>
-      </section>
-    </>
+    <section
+      className="flex flex-col items-center justify-center text-center mt-[90px] text-[var(--light-cream-100)] bg-cover h-[400px] rounded-[12px] bg-[url(/assets/home/coffee-cup-home.jpg)] md:items-start md:text-left md:p-[58px] md:h-[450px]"
+    >
+      <div className="translate-y-[-10%] m-6 max-w-[398px] md:max-w-none md:mb-8 md:mt-6">
+        <h1 className="mb-5 md:mb-8 text-white">Great simple coffee.</h1>
+        <p className="text-justify max-w-[398px] md:max-w-[445px] text-white">Good communication is just as stimulating as black coffee, and just as hard to sleep after.</p>
+      </div>
+    </section>
   );
 };
 
-Hero.defaultProps = {
-  imageClassName: "white-image",
-  title: "NextJS Coffee Shop",
-  description: "We are coming soon !",
-};
-
-export default Hero;
+export default Hero
